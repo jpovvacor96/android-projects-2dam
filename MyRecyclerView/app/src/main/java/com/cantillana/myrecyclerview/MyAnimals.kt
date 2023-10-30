515f24171c72c22011029345abd008d2bacf71cf
jpovvacor96 <jpovvacor96@gmail.com>
2023-10-30 18:11:47 +0100

    Pequeñas modificaciones en el proyecto con RecyclerView

--- MyRecyclerView/app/src/main/java/com/cantillana/myrecyclerview/MyAnimals.kt
+++ MyRecyclerView/app/src/main/java/com/cantillana/myrecyclerview/MyAnimals.kt
@@ -1,14 +1,14 @@
 package com.cantillana.myrecyclerview
 
-data class MyAnimals(val name:String, val latin:String, val image:Int){
+data class MyAnimals(val name: String, val latin: String, val image: Int) {
 
-    var animalName: String?=null
-    var latinName: String?=null
-    var imageAnimal: Int=0
+    var animalName: String? = null
+    var latinName: String? = null
+    var imageAnimal: Int = 0
 
     init {
-        this.animalName=name
-        this.latinName=latin
-        this.imageAnimal=image
+        this.animalName = name
+        this.latinName = latin
+        this.imageAnimal = image
     }
 }
